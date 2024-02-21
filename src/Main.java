d3f527fde1161173119e4b659db0f5782bc27a10
lucas.catunda <lucas.catunda@cinbesa.com.br>
2024-02-21 09:59:45 -0300

    feature: switch case refatorado e fim do código imprime agora apenas de trás pra frente

--- src/Main.java
+++ src/Main.java
@@ -15,32 +15,30 @@ public class Main {
                 System.out.println("Deseja adicionar ao inicio(APERTE 1), no fim(APERTE 2)? ou remover um elemento?(APERTE 3)");
                 int operacao = scanner.nextInt();
                 int numero;
-                switch (operacao){
-                    case 1:
+                switch (operacao) {
+                    case 1 -> {
                         System.out.println("Digite o número para adicionar ao inicio da lista:");
                         numero = scanner.nextInt();
                         lista.adicionarNoInicio(numero);
                         lista.imprimirLista();
-                        break;
-                    case 2:
+                    }
+                    case 2 -> {
                         System.out.println("Digite o número para adicionar ao fim da lista:");
                         numero = scanner.nextInt();
                         lista.adicionarNoFim(numero);
                         lista.imprimirLista();
-                        break;
-
-                    case 3:
+                    }
+                    case 3 -> {
                         System.out.println("Digite um elemento que deseja remover da lista:");
                         numero = scanner.nextInt();
                         lista.removeElementoDaLista(numero);
                         lista.imprimirLista();
-                        break;
-                    default:
-                        break;
+                    }
+                    default -> {
+                    }
                 }
             }
         }while(!adicionar.equals("N"));
-        lista.imprimirLista();
         lista.imprimirDeTrasParaFrente();
         lista.esvaziarLista();
         lista.imprimirLista();
